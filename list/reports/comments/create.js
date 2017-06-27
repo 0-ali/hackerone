dc90b67e69320573bb70428e448b7b5314c06476
xc0d3rz <x.c0d3rz000@gmail.com>
2017-06-28 01:10:13 +0300

    Add reports.comments.create

---
+++ list/reports/comments/create.js
@@ -0,0 +1,26 @@
+/*!
+ * hackerone.reports.comment.create (https://github.com/xc0d3rz/hackerone)
+ * Copyright 2016-2017 xc0d3rz(x.c0d3rz000@gmail.com)
+ * Licensed under the MIT license
+ */
+
+/**
+ *
+ * @param id
+ * @param param
+ * @param callback
+ */
+module.exports = function (id, param, callback) {
+    if (param.hasOwnProperty('message') && param.hasOwnProperty('internal')) {
+
+        this.post('reports/' + id + '/activities', {
+            data: {
+                type: "activity-comment",
+                attributes: {
+                    message: param.message,
+                    internal: param.internal
+                }
+            }
+        }, callback);
+    }
+};
