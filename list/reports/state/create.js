0a5381b5198248f0958cd0f9bd91c9878e34dcda
xc0d3rz <x.c0d3rz000@gmail.com>
2017-06-28 16:30:19 +0300

    Add reports.state.create

---
+++ list/reports/state/create.js
@@ -0,0 +1,24 @@
+/*!
+ * hackerone.reports.state.create (https://github.com/xc0d3rz/hackerone)
+ * Copyright 2016-2017 xc0d3rz(x.c0d3rz000@gmail.com)
+ * Licensed under the MIT license
+ */
+
+/**
+ *
+ * @param id
+ * @param param
+ * @param callback
+ */
+module.exports = function (id, param, callback) {
+    this.post('reports/' + id + '/assignee', {
+        data: {
+            id: param.id,
+            type: 'state-change',
+            attributes: {
+                message: param.message,
+                state: param.state
+            }
+        }
+    }, callback);
+};
