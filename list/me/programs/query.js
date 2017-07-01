67e8f53ae13b2394dfad03fa3ee8a5d43883bb9f
xc0d3rz <x.c0d3rz000@gmail.com>
2017-07-01 11:59:29 +0300

    Add me.programs.query

---
+++ list/me/programs/query.js
@@ -0,0 +1,19 @@
+/*!
+ * hackerone.me.programs.query (https://github.com/xc0d3rz/hackerone)
+ * Copyright 2016-2017 xc0d3rz(x.c0d3rz000@gmail.com)
+ * Licensed under the MIT license
+ */
+
+/**
+ *
+ * @param query
+ * @param callback
+ */
+module.exports = function (query, callback) {
+    var param = {page: {}};
+    query = query || {};
+    for (var o in query) {
+        param.page[o] = [query[o]];
+    }
+    this.get('me/programs', param, callback);
+};
