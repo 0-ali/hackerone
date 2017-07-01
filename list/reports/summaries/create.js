587d891cb997a2bfc521da5a80fa1bdd8d80675d
xc0d3rz <x.c0d3rz000@gmail.com>
2017-07-01 11:16:36 +0300

    Add reports.summaries.create

---
+++ list/reports/summaries/create.js
@@ -0,0 +1,22 @@
+/*!
+ * hackerone.reports.summaries.create (https://github.com/xc0d3rz/hackerone)
+ * Copyright 2016-2017 xc0d3rz(x.c0d3rz000@gmail.com)
+ * Licensed under the MIT license
+ */
+
+/**
+ *
+ * @param id
+ * @param param
+ * @param callback
+ */
+module.exports = function (id, param, callback) {
+    this.post('reports/' + id + '/summaries', {
+        data: {
+            type: 'report-summary',
+            attributes: {
+                content: param.content
+            }
+        }
+    }, callback);
+};
