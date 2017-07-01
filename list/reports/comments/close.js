486fecc0ebb741b6d309a89490d8a2be6f11e4a8
xc0d3rz <x.c0d3rz000@gmail.com>
2017-07-01 11:07:30 +0300

    Add reports.comments.close

---
+++ list/reports/comments/close.js
@@ -0,0 +1,22 @@
+/*!
+ * hackerone.reports.comment.create (https://github.com/xc0d3rz/hackerone)
+ * Copyright 2016-2017 xc0d3rz(x.c0d3rz000@gmail.com)
+ * Licensed under the MIT license
+ */
+
+/**
+ *
+ * @param id
+ * @param param
+ * @param callback
+ */
+module.exports = function (id, param, callback) {
+    this.post('reports/' + id + '/close_comments', {
+        data: {
+            type: 'activity-comments-closed',
+            attributes: {
+                id: param.id
+            }
+        }
+    }, callback);
+};
