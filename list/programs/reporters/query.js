db71be670531b384487f027c42f690cbc8a83743
xc0d3rz <x.c0d3rz000@gmail.com>
2017-07-01 11:39:10 +0300

    Add programs.reporters.query

---
+++ list/programs/reporters/query.js
@@ -0,0 +1,14 @@
+/*!
+ * hackerone.programs.reporters.query (https://github.com/xc0d3rz/hackerone)
+ * Copyright 2016-2017 xc0d3rz(x.c0d3rz000@gmail.com)
+ * Licensed under the MIT license
+ */
+
+/**
+ *
+ * @param id
+ * @param callback
+ */
+module.exports = function (id, callback) {
+    this.get('programs/' + id + '/reporters', {}, callback);
+};
