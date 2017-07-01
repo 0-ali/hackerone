bb63156ca51c2ecd4b660022bcbfd471d8d563fe
xc0d3rz <x.c0d3rz000@gmail.com>
2017-07-01 10:54:52 +0300

    Add new tests & cleanup

--- test/reports.js
+++ test/reports.js
@@ -4,16 +4,18 @@ var hackerone = require('../index');
 var Hackerone = new hackerone('api_example_company', 'Ke+2jinhe5jM87P95aAVOz7L3ZWrtSiERtyOkkh5tEQ=');
 
 describe('Reports', function () {
-    describe('Read', function () {
-        it('returns report id', function (done) {
+
+    describe('Read a report', function () {
+        it('respond with a report object', function (done) {
             Hackerone.reports.read(129329, function (err, res) {
                 assert.equal(res.data.id, 129329);
                 done();
             });
         });
     });
-    describe('Query', function () {
-        it('returns no error', function (done) {
+
+    describe('Query reports', function () {
+        it('respond with paginated report objects', function (done) {
             Hackerone.reports.query({
                 program: 'john_doe_example_company'
             }, function (err, res) {
@@ -24,7 +26,7 @@ describe('Reports', function () {
     });
 
     describe('Comments', function () {
-        describe('Create', function () {
+        describe('Post a public comment', function () {
             it('returns no error', function (done) {
                 var _this = this;
                 Hackerone.reports.comments.create(129329, {
@@ -43,13 +45,33 @@ describe('Reports', function () {
     });
 
     describe('Assignee', function () {
-        describe('Update', function () {
-            it('returns no error', function (done) {
+        describe('Assign a user', function () {
+            it('respond with the updated report object', function (done) {
                 var _this = this;
                 Hackerone.reports.assignee.update(129329, {
                     id: 1337,
                     type: 'user',
                     message: '@member Please check this out!'
+                }, function (err, res) {
+                    if (err.errors[0].status == 403) {
+                        _this.skip();
+                    } else {
+                        assert.equal(err, null);
+                    }
+                    done();
+                });
+
+            });
+        });
+    });
+
+    describe('State', function () {
+        describe('Mark a report as resolved', function () {
+            it('respond without any error', function (done) {
+                var _this = this;
+                Hackerone.reports.state.create(129329, {
+                    message: 'This vulnerability has been resolved. Thanks!',
+                    state: 'resolved'
                 }, function (err, res) {
                     if (err.errors[0].status == 403) {
                         _this.skip();
